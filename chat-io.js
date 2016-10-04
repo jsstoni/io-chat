353087fad7a1bbc287578ac497830f89adb74a84
jsstoni <counsantor2d@gmail.com>
2016-10-04 15:39:20 -0430

    express

--- chat-io.js
+++ chat-io.js
@@ -1,9 +1,23 @@
-const io = require('socket.io')()
+const express = require('express')
+const app = module.exports = express()
+const path = require('path')
+const port = process.env.PORT || 8080
+
+app.set("view options", {layout: false})
+app.use(express.static(path.join(__dirname, 'client-view')))
+
+app.get('/', (req, res) => {
+	res.render('index.html')
+})
+
+const server = app.listen(port, () => {
+	console.log('Server listening on port ' + port)
+})
+
+const io = require('socket.io')(server)
 
 io.on('connection', (socket) => {
 	socket.on('recibe', (data) => {
 		socket.emit('message', data)
 	})
 })
-
-io.listen(60000)
